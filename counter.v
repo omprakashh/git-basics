5cdfc768473886a68196861f2fa49f83f2c4d995
Om Prakash Hari <omprakashh@outlook.com>
2018-05-27 08:35:04 +0530

    Add initial design and README

---
+++ counter.v
@@ -0,0 +1,18 @@
+// 4-bit Wrapping Counter
+module counter
+(
+    input  wire       clock, 
+    input  wire       resetn, 
+    output wire [3:0] count
+);
+
+always@(posedge clock, negedge resetn) begin
+    if(!resetn) begin
+        count <= 4'h0;
+    end 
+    else begin
+        count <= count + 4'h1;
+    end
+end
+
+endmodule
